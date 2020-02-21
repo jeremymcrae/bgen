41ba9b207099ab6abed32e51f8a1bb4a274050de
Jeremy McRae <jmcrae@illumina.com>
2020-02-20 16:34:57 -0800

    fix too many spaces

--- src/header.cpp
+++ src/header.cpp
@@ -9,10 +9,10 @@
 namespace bgen {
 
 unsigned char reverse(unsigned char b) {
-   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-   return b;
+  b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+  b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+  return b;
 }
 
 std::bitset<32> read_flags(std::ifstream & handle) {
