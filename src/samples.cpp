57899322a768474a05fc64978a259aa58354bc88
Jeremy McRae <jmcrae@illumina.com>
2020-02-26 10:19:26 -0800

    added method to load sampels from external file

--- src/samples.cpp
+++ src/samples.cpp
@@ -9,6 +9,8 @@
 namespace bgen {
 
 BgenSamples::BgenSamples(std::ifstream & handle, int n_samples) {
+  /* initialize sample list if present in the bgen file
+  */
   std::uint32_t sample_header_length;
   handle.read(reinterpret_cast<char*>(&sample_header_length), sizeof(std::uint32_t));
   
@@ -24,6 +26,35 @@ BgenSamples::BgenSamples(std::ifstream & handle, int n_samples) {
     std::string sample_id;
     std::copy_n(std::istream_iterator<char>(handle), id_len, std::back_inserter(sample_id));
     samples.push_back(sample_id);
+  }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+  /* split a string by delimiter into a vector of elements
+  */
+  std::vector<std::string> elems;
+  std::istringstream iss(s);
+  std::string item;
+  while (std::getline(iss, item, delim)) {
+    elems.push_back(item);
+  }
+  return elems;
+}
+
+BgenSamples::BgenSamples(std::string path, int n_samples) {
+  /* initialize from external sample file
+  */
+  std::ifstream handle(path, std::ios::in);
+  
+  std::string line;
+  while (std::getline(handle, line)) {
+    std::vector<std::string> elems = split(line, '\t');
+    auto sample_id = elems[0];
+    if ((sample_id == "FID") | (sample_id == "0")) { continue; } // skip header lines
+    samples.push_back(sample_id);
+  }
+  if (n_samples != (int)samples.size()) {
+    throw std::invalid_argument("inconsistent number of samples");
   }
 }
 
--- src/samples.h
+++ src/samples.h
@@ -2,12 +2,14 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 namespace bgen {
 
 class BgenSamples {
 public:
   BgenSamples(std::ifstream & handle, int n_samples);
+  BgenSamples(std::string path, int n_samples);
   BgenSamples() {};
   std::vector<std::string> samples;
 };
