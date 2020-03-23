d9d77a650d416be4c33a94a93e57bc552aef701d
Jeremy McRae <jmcrae@illumina.com>
2020-03-23 10:36:12 -0700

    start on getting zstd to compile as shared library

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 
 import io
+from pathlib import Path
 from setuptools import setup
 import sys
+
 from distutils.core import Extension
-from Cython.Build import cythonize
+from Cython.Build import cythonize, build_ext
 
 EXTRA_COMPILE_ARGS = ['-std=c++11']
 EXTRA_LINK_ARGS = []
@@ -11,7 +13,61 @@ if sys.platform == "darwin":
     EXTRA_COMPILE_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
     EXTRA_LINK_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
 
+def flatten(*lists):
+    return [str(x) for sublist in lists for x in sublist]
+
+folder = Path('src/zstd/lib')
+zstd = Extension('bgen.libzstd',
+    extra_compile_args=["-std=gnu11"],
+    extra_link_args=['-shared',],
+    sources=flatten(
+        (folder / 'common').glob('*.c'),
+        (folder / 'compress').glob('*.c'),
+        (folder / 'dictBuilder').glob('*.c'),
+        (folder / 'deprecated').glob('*.c'),
+        (folder / 'legacy').glob('*.c'),  # TODO: drop some legacy versions
+    ),
+    include_dirs=['src/zstd/lib/',
+        'src/zstd/lib/common',
+    ],
+    language='c'),
+builder = build_ext('temp')
+print(dir(zstd[0]))
+builder.build_extension(zstd[0])
+
+
+# ext_libraries = [['zstd', {
+#     'extra_compile_args': ["-std=gnu11"],
+#     # 'extra_link_args': ['-shared'],
+#     'sources':  flatten(
+#         (folder / 'common').glob('*.c'),
+#         (folder / 'compress').glob('*.c'),
+#         (folder / 'dictBuilder').glob('*.c'),
+#         (folder / 'deprecated').glob('*.c'),
+#         (folder / 'legacy').glob('*.c'),  # TODO: drop some legacy versions
+#     ),
+#     'include_dirs': ['src/zstd/lib/',
+#         'src/zstd/lib/common',
+#     ],
+#     'language': 'c'
+#     }
+# ]]
+
 reader = cythonize([
+    # Extension('bgen.libzstd',
+    #     extra_compile_args=["-std=gnu11"],
+    #     extra_link_args=['-shared',],
+    #     sources=flatten(
+    #         (folder / 'common').glob('*.c'),
+    #         (folder / 'compress').glob('*.c'),
+    #         (folder / 'dictBuilder').glob('*.c'),
+    #         (folder / 'deprecated').glob('*.c'),
+    #         (folder / 'legacy').glob('*.c'),  # TODO: drop some legacy versions
+    #     ),
+    #     include_dirs=['src/zstd/lib/',
+    #         'src/zstd/lib/common',
+    #     ],
+    #     language='c'),
     Extension('bgen.reader',
         extra_compile_args=EXTRA_COMPILE_ARGS,
         extra_link_args=EXTRA_LINK_ARGS,
@@ -23,7 +79,9 @@ reader = cythonize([
             'src/utils.cpp',
             'src/variant.cpp'],
         include_dirs=['src/', 'src/zstd/lib'],
-        libraries=['z'],
+        libraries=['z', 'zstd.cpython-37m-darwin'],
+        # library_dirs=[str(x) for x in Path('build').glob('**') if x.is_dir() and 'lib' in str(x)],
+        library_dirs=['bgen'],
         language='c++'),
     ])
 
@@ -43,6 +101,8 @@ setup(name='bgen',
         'Development Status :: 3 - Alpha',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
     ],
+    # ext_modules=[zstd()],
     ext_modules=reader,
+    # libraries=ext_libraries,
     test_suite='tests',
     )
