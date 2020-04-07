c92012bfda680daee143c57a3b4946baa1e4c891
Jeremy McRae <jmcrae@illumina.com>
2020-04-06 17:01:06 -0700

    updated unit tests

--- tests/test_load_example_files.py
+++ tests/test_load_example_files.py
@@ -13,22 +13,30 @@ class TestExampleBgens(unittest.TestCase):
         self.folder = Path(__file__).parent /  "data"
     
     def test_load_example_files(self):
-        ''' make sure we can load all the example files
+        ''' check we can open the example files
         '''
         for path in self.folder.glob('example*.bgen'):
             bfile = BgenFile(str(path))
     
     def test_load_example_genotypes(self):
-        ''' make sure we can load all the example files
+        ''' check parsing genotypes from the example files
         '''
         for path in self.folder.glob('example*.bgen'):
+            print(f'testing {path}')
             bfile = BgenFile(str(path))
             for var in bfile:
-                # geno = var.probabilities
-                pass
+                geno = var.probabilities
+    
+    def test_zstd_compressed(self):
+        ''' check we can parse genotypes from zstd compressed geno probabilities
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        bfile = BgenFile(str(path))
+        for var in bfile:
+            geno = var.probabilities
     
     def test_load_complex_files(self):
-        ''' make sure we can load all the complex bgen files
+        ''' make sure we can open the complex bgen files
         '''
         for path in self.folder.glob('complex*.bgen'):
             # bfile = BgenFile(str(path))
@@ -39,3 +47,7 @@ class TestExampleBgens(unittest.TestCase):
         '''
         with self.assertRaises(ValueError):
             BgenFile('/zzz/jjj/qqq.bgen')
+    
+    # def test_load_missing_sample_file(self):
+    #     path = str(self.folder / 'example.8bits.bgen')
+    #     bfile = BgenFile(path, '/zzz/jjj/qqq.sample')
