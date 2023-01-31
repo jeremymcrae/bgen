55a58426f462ca1853681162e0f6d24ab6ed114f
Jeremy McRae <jmcrae@illumina.com>
2023-01-30 16:25:57 -0800

    better formatting

--- src/genotypes.h
+++ src/genotypes.h
@@ -33,13 +33,14 @@ class Genotypes {
 public:
   Genotypes(std::ifstream* handle, int lay, int compr, int n_alleles, std::uint32_t n_samples) :
     handle(handle), layout(lay), compression(compr), n_alleles(n_alleles), n_samples(n_samples) {
+      std::cout << "initializing Genotypes for var at: " << offset << std::endl;
       std::uint32_t length;
       offset = handle->tellg();
-      std::cout << "initial offset: " << offset << std::endl;
+      std::cout << " - initial offset: " << offset << std::endl;
       handle->read(reinterpret_cast<char*>(&length), sizeof(length));
       offset = handle->tellg();
       next_var_offset = offset + length;
-      std::cout << "var offset: " << offset << ", next var offset: " << next_var_offset << std::endl;
+      std::cout << " - var offset: " << offset << ", next var offset: " << next_var_offset << std::endl;
   };
   Genotypes() {};
   ~Genotypes() { clear_probs(); };
