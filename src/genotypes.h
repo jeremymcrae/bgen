813894ed79d7f75ecca24fdf2c067ecbecab9211
Jeremy McRae <jmcrae@illumina.com>
2023-01-31 09:37:42 -0800

    add offset back in

--- src/genotypes.h
+++ src/genotypes.h
@@ -33,7 +33,9 @@ class Genotypes {
   std::vector<int> missing;
 public:
   Genotypes(std::ifstream* handle, int lay, int compr, int n_alleles, std::uint32_t n_samples, std::uint32_t length) :
-     handle(handle), layout(lay), compression(compr), n_alleles(n_alleles), n_samples(n_samples), length(length) {};
+     handle(handle), layout(lay), compression(compr), n_alleles(n_alleles), n_samples(n_samples), length(length) {
+        offset = handle->tellg();
+     };
   Genotypes() {};
   ~Genotypes() { clear_probs(); };
   void parse_preamble(char * uncompressed, std::uint32_t & idx);
