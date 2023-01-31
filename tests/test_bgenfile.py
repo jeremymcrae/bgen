e78c1beb6c81674704e54473e07025306166bc90
Jeremy McRae <jmcrae@illumina.com>
2023-01-31 09:46:00 -0800

    put all tests back in

--- tests/test_bgenfile.py
+++ tests/test_bgenfile.py
@@ -21,137 +21,137 @@ class TestBgenFile(unittest.TestCase):
         '''
         self.folder = Path(__file__).parent /  "data"
     
-    # def test_context_handler_closed_bgen_samples(self):
-    #     ''' no samples available from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile.samples) > 0)
+    def test_context_handler_closed_bgen_samples(self):
+        ''' no samples available from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile.samples) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #         bfile.samples
+        with self.assertRaises(ValueError):
+            bfile.samples
     
-    # def test_context_handler_closed_bgen_varids(self):
-    #     ''' no varids available from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile.varids()) > 0)
+    def test_context_handler_closed_bgen_varids(self):
+        ''' no varids available from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile.varids()) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #         bfile.varids()
+        with self.assertRaises(ValueError):
+            bfile.varids()
     
-    # def test_context_handler_closed_bgen_rsids(self):
-    #     ''' no rsids available from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile.rsids()) > 0)
+    def test_context_handler_closed_bgen_rsids(self):
+        ''' no rsids available from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile.rsids()) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #         bfile.rsids()
+        with self.assertRaises(ValueError):
+            bfile.rsids()
     
-    # def test_context_handler_closed_bgen_positions(self):
-    #     ''' no positions available from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile.positions()) > 0)
+    def test_context_handler_closed_bgen_positions(self):
+        ''' no positions available from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile.positions()) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #         bfile.positions()
+        with self.assertRaises(ValueError):
+            bfile.positions()
     
-    # def test_context_handler_closed_bgen_length(self):
-    #     ''' error raised if accessing length of exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile) > 0)
+    def test_context_handler_closed_bgen_length(self):
+        ''' error raised if accessing length of exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #          len(bfile)
+        with self.assertRaises(ValueError):
+             len(bfile)
     
-    # def test_context_handler_closed_bgen_slice(self):
-    #     ''' error raised if slicing variant from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile) > 0)
+    def test_context_handler_closed_bgen_slice(self):
+        ''' error raised if slicing variant from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #          var = bfile[0]
+        with self.assertRaises(ValueError):
+             var = bfile[0]
     
-    # def test_context_handler_closed_bgen_at_position(self):
-    #     ''' error raised if getting variant at position from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile) > 0)
+    def test_context_handler_closed_bgen_at_position(self):
+        ''' error raised if getting variant at position from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #          var = bfile.at_position(100)
+        with self.assertRaises(ValueError):
+             var = bfile.at_position(100)
     
-    # def test_context_handler_closed_bgen_with_rsid(self):
-    #     ''' error raised if getting variant with rsid from exited BgenFile
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     with BgenFile(path) as bfile:
-    #         self.assertTrue(len(bfile) > 0)
+    def test_context_handler_closed_bgen_with_rsid(self):
+        ''' error raised if getting variant with rsid from exited BgenFile
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        with BgenFile(path) as bfile:
+            self.assertTrue(len(bfile) > 0)
         
-    #     with self.assertRaises(ValueError):
-    #          var = bfile.with_rsid('rs111')
+        with self.assertRaises(ValueError):
+             var = bfile.with_rsid('rs111')
     
-    # def test_fetch(self):
-    #     ''' can fetch variants within a genomic region
-    #     '''
-    #     chrom, start, stop = '01', 5000, 50000
-    #     bfile = BgenFile(self.folder / 'example.16bits.bgen')
-    #     self.assertTrue(bfile._check_for_index(str(self.folder / 'example.16bits.bgen')))
+    def test_fetch(self):
+        ''' can fetch variants within a genomic region
+        '''
+        chrom, start, stop = '01', 5000, 50000
+        bfile = BgenFile(self.folder / 'example.16bits.bgen')
+        self.assertTrue(bfile._check_for_index(str(self.folder / 'example.16bits.bgen')))
         
-    #     self.assertTrue(list(bfile.fetch('02')) == [])
+        self.assertTrue(list(bfile.fetch('02')) == [])
     
-    # def test_fetch_whole_chrom(self):
-    #     ''' fetching just with chrom gives all variants on chromosome
-    #     '''
-    #     chrom, start, stop = '01', 5000, 50000
-    #     bfile = BgenFile(self.folder / 'example.16bits.bgen')
+    def test_fetch_whole_chrom(self):
+        ''' fetching just with chrom gives all variants on chromosome
+        '''
+        chrom, start, stop = '01', 5000, 50000
+        bfile = BgenFile(self.folder / 'example.16bits.bgen')
         
-    #     # test fetching a whole chromosome
-    #     sortkey = lambda x: (x.chrom, x.pos)
-    #     for x, y in zip(sorted(bfile.fetch(chrom), key=sortkey), sorted(self.gen_data, key=sortkey)):
-    #         self.assertEqual(x.rsid, y.rsid)
-    #         self.assertEqual(x.chrom, y.chrom)
-    #         self.assertEqual(x.pos, y.pos)
+        # test fetching a whole chromosome
+        sortkey = lambda x: (x.chrom, x.pos)
+        for x, y in zip(sorted(bfile.fetch(chrom), key=sortkey), sorted(self.gen_data, key=sortkey)):
+            self.assertEqual(x.rsid, y.rsid)
+            self.assertEqual(x.chrom, y.chrom)
+            self.assertEqual(x.pos, y.pos)
     
-    # def test_fetch_after_position(self):
-    #     ''' fetching variants with chrom and start gives all variants after pos
-    #     '''
-    #     chrom, start, stop = '01', 5000, 50000
-    #     print('testing fetch after position')
-    #     bfile = BgenFile(self.folder / 'example.16bits.bgen')
+    def test_fetch_after_position(self):
+        ''' fetching variants with chrom and start gives all variants after pos
+        '''
+        chrom, start, stop = '01', 5000, 50000
+        print('testing fetch after position')
+        bfile = BgenFile(self.folder / 'example.16bits.bgen')
         
-    #     print('opened gen')
-    #     sortkey = lambda x: (x.chrom, x.pos)
-    #     gen_vars = [x for x in sorted(self.gen_data, key=sortkey) if start <= x.pos]
-    #     print('have expected data')
-    #     for x, y in zip(sorted(bfile.fetch(chrom, start), key=sortkey), gen_vars):
-    #         print(x, y)
-    #         self.assertEqual(x.rsid, y.rsid)
-    #         self.assertEqual(x.chrom, y.chrom)
-    #         self.assertEqual(x.pos, y.pos)
+        print('opened gen')
+        sortkey = lambda x: (x.chrom, x.pos)
+        gen_vars = [x for x in sorted(self.gen_data, key=sortkey) if start <= x.pos]
+        print('have expected data')
+        for x, y in zip(sorted(bfile.fetch(chrom, start), key=sortkey), gen_vars):
+            print(x, y)
+            self.assertEqual(x.rsid, y.rsid)
+            self.assertEqual(x.chrom, y.chrom)
+            self.assertEqual(x.pos, y.pos)
     
-    # def test_fetch_in_region(self):
-    #     ''' fetching variants with chrom, start, stop gives variants in region
-    #     '''
-    #     chrom, start, stop = '01', 5000, 50000
-    #     bfile = BgenFile(self.folder / 'example.16bits.bgen')
+    def test_fetch_in_region(self):
+        ''' fetching variants with chrom, start, stop gives variants in region
+        '''
+        chrom, start, stop = '01', 5000, 50000
+        bfile = BgenFile(self.folder / 'example.16bits.bgen')
         
-    #     sortkey = lambda x: (x.chrom, x.pos)
-    #     gen_vars = [x for x in sorted(self.gen_data, key=sortkey) if start <= x.pos <= stop]
-    #     for x, y in zip(sorted(bfile.fetch(chrom, start, stop), key=sortkey), gen_vars):
-    #         self.assertEqual(x.rsid, y.rsid)
-    #         self.assertEqual(x.chrom, y.chrom)
-    #         self.assertEqual(x.pos, y.pos)
+        sortkey = lambda x: (x.chrom, x.pos)
+        gen_vars = [x for x in sorted(self.gen_data, key=sortkey) if start <= x.pos <= stop]
+        for x, y in zip(sorted(bfile.fetch(chrom, start, stop), key=sortkey), gen_vars):
+            self.assertEqual(x.rsid, y.rsid)
+            self.assertEqual(x.chrom, y.chrom)
+            self.assertEqual(x.pos, y.pos)
         
-    #     # check that we don't get any variants in a region without any
-    #     self.assertEqual(list(bfile.fetch(chrom, start * 1000, stop * 1000)), [])
+        # check that we don't get any variants in a region without any
+        self.assertEqual(list(bfile.fetch(chrom, start * 1000, stop * 1000)), [])
--- tests/test_load_example_files.py
+++ tests/test_load_example_files.py
@@ -22,79 +22,79 @@ class TestExampleBgens(unittest.TestCase):
         '''
         self.folder = Path(__file__).parent /  "data"
     
-    # def test_load_example_genotypes_bit_depths(self):
-    #     ''' check parsing genotypes from the example files with different bit depths
-    #     '''
-    #     for path in self.folder.glob('example.*bits.bgen'):
-    #         bit_depth = int(path.stem.split('.')[1].strip('bits'))
-    #         bfile = BgenFile(str(path))
-    #         for var, g in zip(bfile, self.gen_data):
-    #             self.assertEqual(g, var)
-    #             self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
+    def test_load_example_genotypes_bit_depths(self):
+        ''' check parsing genotypes from the example files with different bit depths
+        '''
+        for path in self.folder.glob('example.*bits.bgen'):
+            bit_depth = int(path.stem.split('.')[1].strip('bits'))
+            bfile = BgenFile(str(path))
+            for var, g in zip(bfile, self.gen_data):
+                self.assertEqual(g, var)
+                self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
     
-    # def test_zstd_compressed(self):
-    #     ''' check we can parse genotypes from zstd compressed geno probabilities
-    #     '''
-    #     path = self.folder / 'example.16bits.zstd.bgen'
-    #     bfile = BgenFile(str(path))
-    #     for var, g in zip(bfile, self.gen_data):
-    #         self.assertEqual(g, var)
-    #         self.assertTrue(arrays_equal(g.probabilities, var.probabilities, 16))
+    def test_zstd_compressed(self):
+        ''' check we can parse genotypes from zstd compressed geno probabilities
+        '''
+        path = self.folder / 'example.16bits.zstd.bgen'
+        bfile = BgenFile(str(path))
+        for var, g in zip(bfile, self.gen_data):
+            self.assertEqual(g, var)
+            self.assertTrue(arrays_equal(g.probabilities, var.probabilities, 16))
     
-    # def test_v11(self):
-    #     ''' check we can open a bgen in v1.1 format, and parse genotypes correctly
-    #     '''
-    #     path = self.folder / 'example.v11.bgen'
-    #     bfile = BgenFile(str(path))
-    #     bit_depth = 16
-    #     for var, g in zip(bfile, self.gen_data):
-    #         self.assertEqual(g, var)
-    #         self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
+    def test_v11(self):
+        ''' check we can open a bgen in v1.1 format, and parse genotypes correctly
+        '''
+        path = self.folder / 'example.v11.bgen'
+        bfile = BgenFile(str(path))
+        bit_depth = 16
+        for var, g in zip(bfile, self.gen_data):
+            self.assertEqual(g, var)
+            self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
     
-    # def test_Path(self):
-    #     ''' check we can open bgen files from Path objects
-    #     '''
-    #     path = self.folder / 'example.v11.bgen'
-    #     bfile = BgenFile(path)
+    def test_Path(self):
+        ''' check we can open bgen files from Path objects
+        '''
+        path = self.folder / 'example.v11.bgen'
+        bfile = BgenFile(path)
     
-    # def test_load_haplotypes_bgen(self):
-    #     ''' check we can open a bgen with haplotypes, and parse genotypes correctly
-    #     '''
-    #     path = self.folder / 'haplotypes.bgen'
-    #     bfile = BgenFile(str(path))
-    #     bit_depth = 16
-    #     for var, g in zip(bfile, self.haps_data):
-    #         self.assertEqual(g, var)
-    #         self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
+    def test_load_haplotypes_bgen(self):
+        ''' check we can open a bgen with haplotypes, and parse genotypes correctly
+        '''
+        path = self.folder / 'haplotypes.bgen'
+        bfile = BgenFile(str(path))
+        bit_depth = 16
+        for var, g in zip(bfile, self.haps_data):
+            self.assertEqual(g, var)
+            self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
     
-    # def test_load_complex_file(self):
-    #     ''' make sure we can open a complex bgen file
-    #     '''
-    #     path = self.folder / 'complex.bgen'
-    #     bfile = BgenFile(path)
-    #     bit_depth = 16
-    #     for var, g in zip(bfile, self.vcf_data):
-    #         self.assertEqual(g, var)
-    #         self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
-    #         self.assertTrue(all(x == y for x, y in zip(g.ploidy, var.ploidy)))
+    def test_load_complex_file(self):
+        ''' make sure we can open a complex bgen file
+        '''
+        path = self.folder / 'complex.bgen'
+        bfile = BgenFile(path)
+        bit_depth = 16
+        for var, g in zip(bfile, self.vcf_data):
+            self.assertEqual(g, var)
+            self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
+            self.assertTrue(all(x == y for x, y in zip(g.ploidy, var.ploidy)))
     
-    # def test_load_complex_files(self):
-    #     ''' make sure we can open the complex bgen files
-    #     '''
+    def test_load_complex_files(self):
+        ''' make sure we can open the complex bgen files
+        '''
         
-    #     for path in self.folder.glob('complex.*.bgen'):
-    #         bit_depth = int(path.stem.split('.')[1].strip('bits'))
-    #         bfile = BgenFile(path)
-    #         for var, g in zip(bfile, self.vcf_data):
-    #             self.assertEqual(g, var)
-    #             self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
+        for path in self.folder.glob('complex.*.bgen'):
+            bit_depth = int(path.stem.split('.')[1].strip('bits'))
+            bfile = BgenFile(path)
+            for var, g in zip(bfile, self.vcf_data):
+                self.assertEqual(g, var)
+                self.assertTrue(arrays_equal(g.probabilities, var.probabilities, bit_depth))
     
-    # def test_load_missing_file(self):
-    #     ''' check passing in a path to a missing file fails gracefully
-    #     '''
-    #     with self.assertRaises(ValueError):
-    #         BgenFile('/zzz/jjj/qqq.bgen')
+    def test_load_missing_file(self):
+        ''' check passing in a path to a missing file fails gracefully
+        '''
+        with self.assertRaises(ValueError):
+            BgenFile('/zzz/jjj/qqq.bgen')
     
-    # # def test_load_missing_sample_file(self):
-    # #     path = str(self.folder / 'example.8bits.bgen')
-    # #     bfile = BgenFile(path, '/zzz/jjj/qqq.sample')
+    # def test_load_missing_sample_file(self):
+    #     path = str(self.folder / 'example.8bits.bgen')
+    #     bfile = BgenFile(path, '/zzz/jjj/qqq.sample')
