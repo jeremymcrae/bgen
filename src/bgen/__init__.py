05fd2ceb82403cdb2c071d539d4963d3ecef0575
Jeremy McRae <jmcrae@illumina.com>
2023-10-24 15:55:41 -0700

    expose BgenVar in package for easier function typing

--- src/bgen/__init__.py
+++ src/bgen/__init__.py
@@ -2,5 +2,5 @@ from pkg_resources import get_distribution
 
 __version__ = get_distribution('bgen').version
 
-from bgen.reader import BgenReader
+from bgen.reader import BgenReader, BgenVar
 from bgen.writer import BgenWriter
