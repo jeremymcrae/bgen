c040b42a4d97edbda046338f8a27cf972bfce991
Jeremy McRae <jmcrae@illumina.com>
2025-09-03 09:45:35 -0700

    avoid avx on macosx, even on x86_64

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ elif sys.platform == "darwin":
 elif sys.platform == "win32":
     EXTRA_COMPILE_ARGS += ['/std:c++14', '/O2']
 
-if platform.machine() == 'x86_64':
+if platform.machine() == 'x86_64' and sys.platform != "darwin":
     EXTRA_COMPILE_ARGS += ['-mavx', '-mavx2']
 
 def flatten(*lists):
